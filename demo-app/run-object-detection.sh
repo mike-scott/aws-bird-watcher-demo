50a98699e831a8fab61df6688cfe4b7d5b2f948d
Michael Scott <mike@foundries.io>
2024-11-26 09:55:53 -0800

    demo-app: update run script artifact naming
    - These match the instructions
    
    Signed-off-by: Michael Scott <mike@foundries.io>

--- demo-app/run-object-detection.sh
+++ demo-app/run-object-detection.sh
@@ -7,8 +7,8 @@ export OBJECT_DETECTION_CHANNEL="Detection::Demo"
 
 exec gst-launch-1.0 -e --gst-debug=2 \
 	qtimlvconverter name=stage_01_preproc mode=image-batch-non-cumulative \
-	qtimlsnpe name=stage_01_inference delegate=dsp model="/opt/gst/yolonas.dlc" layers="</heads/Mul, /heads/Sigmoid>" \
-	qtimlvdetection name=stage_01_postproc threshold=65.0 stabilization=true results=10 module=yolo-nas constants="YoloV5,q-offsets=<3.0>,q-scales=<0.005047998391091824>;" labels="/opt/gst/yolonas.labels" \
+	qtimlsnpe name=stage_01_inference delegate=dsp model="/var/sota/compose-apps/demo-app/yolov5m-int8.dlc" layers="<convolution_63, convolution_72, convolution_81>" \
+	qtimlvdetection name=stage_01_postproc threshold=65.0 stabilization=true results=10 module=yolov5 constants="YoloV5,q-offsets=<3.0>,q-scales=<0.01692184992134571>;" labels="/var/sota/compose-apps/demo-app/CUB_200_2011.labels" \
 	qtiqmmfsrc ! capsfilter caps="video/x-raw(memory:GBM),format=NV12,width=1280,height=720,framerate=30/1,compression=ubwc" ! queue ! tee name=t_split_1 \
 	t_split_1. ! queue ! metamux_1. \
 	t_split_1. ! queue ! stage_01_preproc. stage_01_preproc. ! queue ! stage_01_inference. stage_01_inference. ! queue ! stage_01_postproc. stage_01_postproc. ! text/x-raw ! queue ! metamux_1. \
